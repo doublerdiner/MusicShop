384e86e9efc4770d1bbf7bcd20a90398a34522f8
Chris Barclay <chris.barclay1@gmail.com>
2023-04-21 13:43:51 +0100

    DrumKit set up and tested.

--- src/main/java/musicshop/stock/instruments/DrumKit.java
+++ src/main/java/musicshop/stock/instruments/DrumKit.java
@@ -1,4 +1,25 @@
 package musicshop.stock.instruments;
 
-public class DrumKit {
+public class DrumKit extends Instrument{
+    private String wood;
+    private String colour;
+    private int pieces;
+    public DrumKit(String brand, String model, int pieces, String wood, String colour, double boughtFor) {
+        super(brand, model, InstrumentType.PERCUSSION, boughtFor);
+        this.pieces = pieces;
+        this.wood = wood;
+        this.colour = colour;
+    }
+
+    public int getPieces() {
+        return pieces;
+    }
+
+    public String getWood() {
+        return wood;
+    }
+
+    public String getColour() {
+        return colour;
+    }
 }
---
+++ src/test/java/DrumKitTest.java
@@ -0,0 +1,55 @@
+import musicshop.stock.ISell;
+import musicshop.stock.instruments.DrumKit;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DrumKitTest {
+    private DrumKit drumkit;
+    @Before
+    public void setup(){
+        drumkit = new DrumKit("Tama", "Starclassic", 5, "Birch", "Phantasm Oyster", 2000);
+    }
+    @Test
+    public void drumkitHasBrand(){
+        assertEquals("Tama", drumkit.getBrand());
+    }
+    @Test
+    public void drumkitHasModel(){
+        assertEquals("Starclassic", drumkit.getModel());
+    }
+    @Test
+    public void drumkitHasNumberOfPieces(){
+        assertEquals(5, drumkit.getPieces());
+    }
+    @Test
+    public void drumkitHasWood(){
+        assertEquals("Birch", drumkit.getWood());
+    }
+    @Test
+    public void drumkitHasColour(){
+        assertEquals("Phantasm Oyster", drumkit.getColour());
+    }
+    @Test
+    public void drumkitHasBoughtFor(){
+        assertEquals(2000, drumkit.getBoughtFor(), 0.01);
+    }
+    @Test
+    public void drumkitHasSellFor(){
+        assertEquals(2400, drumkit.getSellFor(), 0.01);
+    }
+    @Test
+    public void drumkitHasInstrumentType(){
+        assertEquals("Percussion", drumkit.getInstrumentType());
+    }
+    @Test
+    public void drumkitHasMarkupMethod(){
+        assertEquals(.2, drumkit.getMarkupPercentage(), 0.01);
+        assertEquals(400, drumkit.calculateMarkup(), 0.01);
+    }
+    @Test
+    public void drumkitIsInstanceOfISell(){
+        assertEquals(true, (drumkit instanceof ISell));
+    }
+}
